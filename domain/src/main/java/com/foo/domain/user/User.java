a56f57e9d9fd708b37d19d63b97b7cceea9665f0
jiangwang3 <jiangwang3@jd.com>
2018-04-20 17:47:37 +0800

    【spring】学习初步搭建

---
+++ domain/src/main/java/com/foo/domain/user/User.java
@@ -0,0 +1,26 @@
+package com.foo.domain.user;
+
+/**
+ * Description:
+ * Created by jiangwang3 on 2018/4/20.
+ */
+public class User {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
