52e0fd4800421b61e5611f73b59f004cdcc60654
jiangwang3 <jiangwang3@jd.com>
2018-04-20 17:57:56 +0800

    【spring】学习初步搭建

---
+++ service/src/main/java/com/foo/service/user/UserService.java
@@ -0,0 +1,31 @@
+package com.foo.service.user;
+
+import com.foo.dao.user.UserDao;
+import com.foo.domain.user.User;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * Description:
+ * Created by jiangwang3 on 2018/4/20.
+ */
+@Service
+public class UserService {
+    @Resource
+    private UserDao userDao;
+
+    public int insert(String name){
+        User user = new User();
+        user.setName(name);
+        return userDao.insertEntry(user);
+    }
+
+    public int selectCount(){
+        return userDao.selectCount();
+    }
+
+    public User selectUserByName(String name){
+        return userDao.selectEntryByName(name);
+    }
+}
