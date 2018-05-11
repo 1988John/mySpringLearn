0ebde8b1e64b7c2fe4f2d9d961fb8aa436befa50
jiangwang3 <jiangwang3@jd.com>
2018-05-11 21:01:38 +0800

    【spring】代理方式探究

---
+++ web/src/test/java/com/foo/test/base/classpackage/Bird.java
@@ -0,0 +1,19 @@
+package com.foo.test.base.classpackage;
+
+import com.foo.test.base.interfacePackage.Jump;
+
+/**
+ * Description:
+ * Created by jiangwang3 on 2018/5/10.
+ */
+public class Bird implements Jump {
+    @Override
+    public void jump() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
---
+++ web/src/test/java/com/foo/test/base/interfacePackage/Action.java
@@ -0,0 +1,9 @@
+package com.foo.test.base.interfacePackage;
+
+/**
+ * Description:
+ * Created by jiangwang3 on 2018/5/10.
+ */
+public interface Action {
+    void fly();
+}
