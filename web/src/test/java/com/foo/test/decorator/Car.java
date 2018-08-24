d989a16ae0c2eb495f7092967bf28c8dc0cf72e4
jiangwang3 <jiangwang3@jd.com>
2018-08-24 14:55:41 +0800

    【design pattern】 decorator

---
+++ web/src/test/java/com/foo/test/decorator/Car.java
@@ -0,0 +1,6 @@
+package com.foo.test.decorator;
+
+public interface Car {
+
+	public void assemble();
+}
---
+++ web/src/test/java/com/foo/test/decorator/DecoratorPatternTest.java
@@ -0,0 +1,15 @@
+package com.foo.test.decorator;
+
+
+public class DecoratorPatternTest {
+
+	public static void main(String[] args) {
+		Car sportsCar = new SportsCar(new BasicCar());
+		sportsCar.assemble();
+		System.out.println("\n*****");
+		
+		Car sportsLuxuryCar = new SportsCar(new LuxuryCar(new BasicCar()));
+		sportsLuxuryCar.assemble();
+	}
+
+}
