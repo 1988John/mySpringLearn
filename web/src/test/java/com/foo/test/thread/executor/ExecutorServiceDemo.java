e0e7f66f0ec75b65bff062cfb6907a75b776f16b
jiangwang3 <jiangwang3@jd.com>
2018-09-04 19:06:39 +0800

    【stack】 基础使用

--- web/src/test/java/com/foo/test/thread/executor/ExecutorServiceDemo.java
+++ web/src/test/java/com/foo/test/thread/executor/ExecutorServiceDemo.java
@@ -3,10 +3,7 @@ package com.foo.test.thread.executor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class ExecutorServiceDemo {
 
@@ -16,6 +13,7 @@ public class ExecutorServiceDemo {
         List<Future<Integer>> results = new ArrayList<Future<Integer>>();
 
         ExecutorService executor = Executors.newFixedThreadPool(2);
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(4);
         for (int i = 0; i < 30; i ++){
             MyTask thisTask = new MyTask(i);
             results.add(executor.submit(thisTask));
