9745594f52688e5b57507c598b397a877cd0aaf5
jiangwang <jiangwang@kuaishou.com>
2019-05-06 17:44:23 +0800

    【排序】list-test

---
+++ web/src/test/java/com/foo/test/sort/Man.java
@@ -0,0 +1,31 @@
+package com.foo.test.sort;
+
+/**
+ * @author fooisart
+ * @date 11:06 06-05-2019
+ */
+public class Man {
+    private int age;
+    private String name;
+
+    public Man(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
