20fcd29ca02294db53a437a70085fc4dd6da72f8
jiangwang <jiangwang@kuaishou.com>
2020-04-13 17:27:02 +0800

    【kie】test

--- web/src/test/java/com/foo/test/MasterGit.java
+++ web/src/test/java/com/foo/test/MasterGit.java
@@ -1,11 +1,18 @@
 package com.foo.test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author jiangwang
  * Created on 02-01-2019
  */
 public class MasterGit {
     public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("a");
+        set.add("b");
+        System.out.println(set);
 
     }
 }
