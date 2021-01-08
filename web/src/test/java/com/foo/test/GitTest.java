0862f7445141cb4a0dd6a04fccdc1f64b17f654c
jiangwang <jiangwang@kuaishou.com>
2021-01-08 16:55:36 +0800

    【mater】构建maste分支

---
+++ web/src/test/java/com/foo/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.foo.test;
+
+/**
+ * @author jiangwang
+ * @date 2021-01-08 16:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("这是master分支");
+    }
+}
