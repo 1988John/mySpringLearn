22b2ae9c30422717e4c765ddb1c1a7bf1df33793
jiangwang <jiangwang@kuaishou.com>
2021-02-20 08:58:43 +0800

    【branch】第五次commit

--- web/src/test/java/com/foo/test/GitTest.java
+++ web/src/test/java/com/foo/test/GitTest.java
@@ -1,5 +1,8 @@
 package com.foo.test;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author jiangwang
  * @date 2021-01-08 16:54
@@ -9,5 +12,7 @@ public class GitTest {
         System.out.println("这是master分支");
         System.out.println("first_rebase_branch-1");
         System.out.println("first_rebase_branch-2");
+        Map<String, String> map = new ConcurrentHashMap<>();
+
     }
 }
